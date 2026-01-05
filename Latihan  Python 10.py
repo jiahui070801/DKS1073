3a0a99e9339f3aa73b641e3c3b5d379231e1eeeb
jiahui070801 <ljiahui546@gmail.com>
2026-01-05 20:27:48 +0800

    Update Latihan  Python 10.py

--- Latihan  Python 10.py
+++ Latihan  Python 10.py
@@ -1,4 +1,5 @@
 Distance = int(input('\nUser enter the distance (in km)'))
 Time = int(input('\nUser enter the travelled the time (in hour)'))
 Average_speed =  Distance/Time
-print(Average_speed, "km/h")
+print(f'Display the average speed:{Average_speed} km/h')
+
