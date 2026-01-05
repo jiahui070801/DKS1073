59b22b4392234aecbdaf6665d0c017f874fa7fb6
jiahui070801 <ljiahui546@gmail.com>
2026-01-05 20:30:10 +0800

    Update Latihan Python 2.py

--- Latihan Python 2.py
+++ Latihan Python 2.py
@@ -1,4 +1,5 @@
 num1 = int(input('\nUser enter the first number:'))
 num2 = int(input('\nUser enter the second number:'))
 sum = num1 + num2
-print(f'Display the number {sum}')
+print(f'Display the number: {sum}')
+
