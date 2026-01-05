e46d1865fdbd71923357a9a34c0bbcb603024a48
jiahui070801 <ljiahui546@gmail.com>
2026-01-05 20:29:16 +0800

    Add files via upload

---
+++ Latihan  Python 3.py
@@ -0,0 +1,8 @@
+num1 = int(input('\nUser enter the first number:'))
+num2 = int(input('\nUser enter the second number:'))
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+print(f'Display the sum:{sum}')
+print(f'Display the difference:{difference}')
+print(f'Display the product:{product}')
