e7ac8e2e05c3d3b9a360f995d08a5bf900e34b8d
jiahui070801 <ljiahui546@gmail.com>
2026-01-05 20:30:29 +0800

    Update Latihan Python 4.py

--- Latihan Python 4.py
+++ Latihan Python 4.py
@@ -1,4 +1,5 @@
 length = int(input('\nUser enter the length of a rectangle:'))
 width = int(input('\nUser enter the width of a rectangle:'))
 area = length * width
-print(f"Display the area for user {area}")
+
+print(f"Display the area for user: {area}")
